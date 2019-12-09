ec0bb31b5dfb6208020f91be300843ba209cbf35
khiem <khiemtd2>
2019-12-09 23:42:59 +0700

    add source basic

--- src/main/java/com/ververica/flinktraining/exercises/datastream_java/basics/RideClean.java
+++ src/main/java/com/ververica/flinktraining/exercises/datastream_java/basics/RideClean.java
@@ -3,15 +3,24 @@ package com.ververica.flinktraining.exercises.datastream_java.basics;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.api.java.utils.ParameterTool;
 
+import com.ververica.flinktraining.exercises.datastream_java.utils.ExerciseBase;
 import com.ververica.flinktraining.exercises.datastream_java.datatypes.TaxiRide;
 import com.ververica.flinktraining.exercises.datastream_java.sources.TaxiRideSource;
 
+
 public class RideClean {
     public static void main(String[] args) {
+        ParameterTool arg = ParameterTool.fromArgs(args);
+        String input  = arg.get("inpur", ExerciseBase.pathToRideData);
+        final int maxEventDelay = 60;       // events are out of order by max 60 seconds
+		final int servingSpeedFactor = 600; // events of 10 minutes are served in 1 second
+
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // source
-        DataStream env.add
+        DataStream<TaxiRide> taxiRide = env.addSource(ExerciseBase.rideSourceOrTest(
+            new TaxiRideSource(input, maxEventDelay, servingSpeedFactor)));
     }
 }
